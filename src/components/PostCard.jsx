a7b1322bbac3d1327dce6c9e2f875f095b2aefd8
GyunHee <ef3232@naver.com>
2023-03-26 01:35:11 +0900

    포스트카드 navigate구현

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { readDatabase } from '../api/firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function PostCard({ post }) {
   const { id, image, category, title, description } = post;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/post/${id}`);
+  };
   return (
-    <li className="rounded-lg shadow-md cursor-pointer">
+    <li onClick={handleClick} className="rounded-lg shadow-md cursor-pointer">
       <img
         className="w-full h-4/5 transition-transform duration-300 hover:scale-105 hover:shadow-slate-700"
         src={image}
